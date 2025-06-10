766881fc7f9e0d1c5cb2e7280adb1bc956f03b5d
Lorenzo <lorenrocu@gmail.com>
2025-06-10 11:48:29 -0500

    RUC y compra completa funcionando.

---
+++ controllers/checkout.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.exceptions import ValidationError
+
+
+class WebsiteSaleCheckout(WebsiteSale):
+    """
+    Controlador personalizado para el checkout con campos DNI/RUC
+    """
+
+    @http.route(['/shop/address'], type='http', auth="public", website=True, sitemap=False)
+    def address(self, **kw):
+        """
+        Sobrescribimos el método address para manejar DNI, RUC y tipo de comprobante
+        """
+        Partner = request.env['res.partner'].with_context(show_address=1).sudo()
+        order = request.website.sale_get_order()
+
+        redirection = self.checkout_redirection(order)
+        if redirection:
+            return redirection
+
+        mode = (False, False)
+        can_edit_vat = False
+        values, errors = {}, {}
+
+        partner_id = int(kw.get('partner_id', -1))
+
+        # IF PUBLIC ORDER
+        if order.partner_id.id == request.website.partner_id.id:
+            mode = ('new', 'billing')
+            can_edit_vat = True
+        # IF ORDER LINKED TO A PARTNER
+        else:
+            if partner_id > 0:
+                if partner_id == order.partner_id.id:
+                    mode = ('edit', 'billing')
+                else:
+                    shippings = Partner.search([('id', 'child_of', order.partner_id.commercial_partner_id.ids)])
+                    if partner_id in shippings.mapped('id'):
+                        mode = ('edit', 'shipping')
+                    else:
+                        return Forbidden()
+                if mode and partner_id != -1:
+                    values = Partner.browse(partner_id)
+            else:
+                mode = ('new', 'shipping')
+            can_edit_vat = order.partner_id.can_edit_vat()
+
+        # IF POSTED
+        if 'submitted' in kw:
+            pre_values = self.values_preprocess(order, mode, kw)
+            errors, error_msg = self.checkout_form_validate(mode, kw, pre_values)
+            post, errors, error_msg = self.values_postprocess(order, mode, pre_values, errors, error_msg)
+
+            if errors:
+                errors['error_message'] = error_msg
+                values = kw
+            else:
+                partner_id = self._checkout_form_save(mode, post, kw)
+                
+                # Asegurar que el partner creado no sea el partner público
+                if partner_id == request.website.partner_id.id:
+                    # Forzar la creación de un nuevo partner único
+                    import time
+                    unique_data = post.copy()
+                    unique_data['email'] = f"forced_unique_{int(time.time())}@temp.local"
+                    partner_id = request.env['res.partner'].sudo().create(unique_data).id
+                
+                if mode[1] == 'billing':
+                    order.partner_id = partner_id
+                    order.with_context(not_self_saleperson=True).onchange_partner_id()
+                    # This is the *only* thing that the front end user will see/edit anyway when choosing billing address
+                    order.partner_invoice_id = partner_id
+                    if not kw.get('use_same'):
+                        kw['callback'] = kw.get('callback') or 'use_billing'
+                elif mode[1] == 'shipping':
+                    order.partner_shipping_id = partner_id
+
+                # TDE FIXME: don't ever do this
+                order.message_partner_ids = [(4, partner_id), (3, request.website.partner_id.id)]
+                
+                # Verificación final antes de continuar
+                if order.partner_id.id == request.website.partner_id.id:
+                    # Si aún es el partner público, forzar cambio
+                    order.partner_id = partner_id
+                    order.partner_invoice_id = partner_id
+                
+                if not errors:
+                    return request.redirect(kw.get('callback') or '/shop/payment')
+
+        country = request.env['res.country']
+        if kw.get('country_id'):
+            country = country.browse(int(kw.get('country_id')))
+        render_values = {
+            'website_sale_order': order,
+            'partner_id': partner_id,
+            'mode': mode,
+            'checkout': values,
+            'can_edit_vat': can_edit_vat,
+            'country': country,
+            'countries': country.search([]),
+            'states': country.state_ids,
+            'error': errors,
+            'callback': kw.get('callback'),
+            'only_services': order and order.only_services or False
+        }
+        return request.render("website_sale.address", render_values)
+
+    def _checkout_form_save(self, mode, checkout, all_values):
+        """
+        Sobrescribimos para manejar los campos personalizados DNI, RUC y tipo de comprobante
+        """
+        Partner = request.env['res.partner']
+        if mode[0] == 'new':
+            # Asegurar que el partner no sea el partner público del website
+            # Generar un email único si no se proporciona
+            if not checkout.get('email'):
+                import time
+                checkout['email'] = f"customer_{int(time.time())}@temp.local"
+            
+            # Asegurar que el partner tenga un nombre válido
+            if not checkout.get('name'):
+                checkout['name'] = 'Cliente'
+            
+            # Crear el partner
+            partner_id = Partner.sudo().create(checkout).id
+            
+            # Verificar que no sea el partner público
+            if partner_id == request.website.partner_id.id:
+                # Si por alguna razón es el mismo, crear uno nuevo con datos únicos
+                checkout['email'] = f"unique_{partner_id}_{int(time.time())}@temp.local"
+                partner_id = Partner.sudo().create(checkout).id
+                
+        elif mode[0] == 'edit':
+            partner_id = int(all_values.get('partner_id', 0))
+            if partner_id:
+                # double check
+                order = request.website.sale_get_order()
+                shippings = Partner.sudo().search([('id', 'child_of', order.partner_id.commercial_partner_id.ids)])
+                if partner_id not in shippings.mapped('id') and partner_id != order.partner_id.id:
+                    return Forbidden()
+                Partner.sudo().browse(partner_id).write(checkout)
+        return partner_id
+
+    def values_preprocess(self, order, mode, values):
+        """
+        Preprocesamos los valores del formulario incluyendo DNI, RUC y tipo de comprobante
+        """
+        new_values = super(WebsiteSaleCheckout, self).values_preprocess(order, mode, values)
+        
+        # Agregar campos personalizados
+        if 'dni' in values:
+            new_values['dni'] = values['dni']
+        if 'ruc' in values:
+            new_values['ruc'] = values['ruc']
+        if 'invoice_type' in values:
+            new_values['invoice_type'] = values['invoice_type']
+        if 'razon_social' in values:
+            new_values['name'] = values['razon_social']  # Mapear razón social al nombre
+            
+        return new_values
+
+    def checkout_form_validate(self, mode, all_form_values, data_values):
+        """
+        Validamos el formulario incluyendo validaciones para DNI y RUC
+        """
+        error = dict()
+        error_message = []
+
+        # Llamar validación original
+        error, error_message = super(WebsiteSaleCheckout, self).checkout_form_validate(mode, all_form_values, data_values)
+
+        # Validaciones personalizadas
+        invoice_type = all_form_values.get('invoice_type', 'boleta')
+        dni = all_form_values.get('dni', '').strip()
+        ruc = all_form_values.get('ruc', '').strip()
+        razon_social = all_form_values.get('razon_social', '').strip()
+
+        if invoice_type == 'boleta':
+            # Para boleta se requiere DNI
+            if not dni:
+                error['dni'] = 'missing'
+                error_message.append('DNI es requerido para boleta')
+            elif len(dni) != 8 or not dni.isdigit():
+                error['dni'] = 'invalid'
+                error_message.append('DNI debe tener exactamente 8 dígitos')
+        elif invoice_type == 'factura':
+            # Para factura se requiere RUC y Razón Social
+            if not ruc:
+                error['ruc'] = 'missing'
+                error_message.append('RUC es requerido para factura')
+            elif len(ruc) != 11 or not ruc.isdigit():
+                error['ruc'] = 'invalid'
+                error_message.append('RUC debe tener exactamente 11 dígitos')
+            
+            if not razon_social:
+                error['razon_social'] = 'missing'
+                error_message.append('Razón Social es requerida para factura')
+
+        return error, error_message
--- models/res_partner.py
+++ models/res_partner.py
@@ -5,14 +5,36 @@ from odoo.exceptions import ValidationError
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # Campo específico para RUC
+    # Campo específico para DNI (personas naturales)
+    dni = fields.Char('DNI', size=8, help="Documento Nacional de Identidad", required=False)
+    
+    # Campo específico para RUC (empresas)
     ruc = fields.Char('RUC', size=11, help="Número de RUC", required=False)
     
     # Campo personalizado para el checkout
     ruc_custom = fields.Char('RUC', size=11, help="RUC personalizado para el checkout", required=False)
     
-    # Deshabilitar el campo VAT
-    vat = fields.Char('NIF/VAT', readonly=True)
+    # Campo para tipo de comprobante
+    invoice_type = fields.Selection([
+        ('boleta', 'Boleta'),
+        ('factura', 'Factura')
+    ], string='Tipo de Comprobante', default='boleta')
+    
+    # Campo VAT que se mapea automáticamente
+    vat = fields.Char('NIF/VAT', readonly=True, help="Se mapea automáticamente desde DNI o RUC")
+
+    @api.constrains('dni')
+    def _check_dni(self):
+        """
+        Validación del DNI:
+        - Debe tener exactamente 8 dígitos
+        - Todos los caracteres deben ser números
+        """
+        for partner in self:
+            if partner.dni:
+                dni = partner.dni.strip()
+                if len(dni) != 8 or not dni.isdigit():
+                    raise ValidationError('El DNI debe tener exactamente 8 dígitos numéricos.')
 
     @api.constrains('ruc', 'ruc_custom')
     def _check_ruc(self):
@@ -29,24 +51,68 @@ class ResPartner(models.Model):
                     if len(ruc) != 11 or not ruc.isdigit():
                         raise ValidationError('El RUC debe tener exactamente 11 dígitos numéricos.')
 
+    @api.constrains('dni', 'ruc', 'invoice_type')
+    def _check_document_consistency(self):
+        """
+        Validación de consistencia entre tipo de comprobante y documento:
+        - Boleta requiere DNI
+        - Factura requiere RUC
+        """
+        for partner in self:
+            if partner.invoice_type == 'boleta' and not partner.dni:
+                raise ValidationError('Para boleta se requiere DNI.')
+            elif partner.invoice_type == 'factura' and not (partner.ruc or partner.ruc_custom):
+                raise ValidationError('Para factura se requiere RUC.')
+
+    def _update_vat_field(self, vals):
+        """
+        Método auxiliar para actualizar el campo VAT según el tipo de comprobante
+        """
+        # Determinar el tipo de comprobante
+        invoice_type = vals.get('invoice_type', getattr(self, 'invoice_type', 'boleta'))
+        
+        if invoice_type == 'factura':
+            # Para factura, usar RUC
+            if 'ruc' in vals and vals['ruc']:
+                vals['vat'] = vals['ruc']
+            elif 'ruc_custom' in vals and vals['ruc_custom']:
+                vals['vat'] = vals['ruc_custom']
+            elif hasattr(self, 'ruc') and self.ruc:
+                vals['vat'] = self.ruc
+            elif hasattr(self, 'ruc_custom') and self.ruc_custom:
+                vals['vat'] = self.ruc_custom
+        else:
+            # Para boleta, usar DNI
+            if 'dni' in vals and vals['dni']:
+                vals['vat'] = vals['dni']
+            elif hasattr(self, 'dni') and self.dni:
+                vals['vat'] = self.dni
+
     @api.model
     def create(self, vals):
         """
-        Al crear un partner, copiar el RUC al campo vat si está presente
+        Al crear un partner, mapear DNI o RUC al campo VAT según el tipo de comprobante
         """
-        if 'ruc' in vals and vals['ruc']:
-            vals['vat'] = vals['ruc']
-        elif 'ruc_custom' in vals and vals['ruc_custom']:
-            vals['vat'] = vals['ruc_custom']
+        # Crear un objeto temporal para usar el método auxiliar
+        temp_partner = self.new(vals)
+        temp_partner._update_vat_field(vals)
         return super(ResPartner, self).create(vals)
 
     @api.multi
     def write(self, vals):
         """
-        Al actualizar un partner, copiar el RUC al campo vat si está presente
+        Al actualizar un partner, mapear DNI o RUC al campo VAT según el tipo de comprobante
         """
-        if 'ruc' in vals and vals['ruc']:
-            vals['vat'] = vals['ruc']
-        elif 'ruc_custom' in vals and vals['ruc_custom']:
-            vals['vat'] = vals['ruc_custom']
-        return super(ResPartner, self).write(vals) 
+        for partner in self:
+            partner._update_vat_field(vals)
+        return super(ResPartner, self).write(vals)
+
+    @api.onchange('invoice_type', 'dni', 'ruc', 'ruc_custom')
+    def _onchange_document_fields(self):
+        """
+        Actualizar el campo VAT cuando cambian los documentos o el tipo de comprobante
+        """
+        vals = {}
+        self._update_vat_field(vals)
+        if 'vat' in vals:
+            self.vat = vals['vat']
