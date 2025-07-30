441298944eeac888e1bc92295aef112ac782ee66
Lorenzo <lorenrocu@gmail.com>
2025-07-30 12:18:42 -0500

    change

--- controllers/confirmation.py
+++ controllers/confirmation.py
@@ -6,9 +6,19 @@ class WebsiteSaleConfirmationCustom(http.Controller):
     def shop_confirmation(self, **kwargs):
         order = request.website.sale_get_order()
         acquirer = None
-        if order and order.payment_acquirer_id:
-            acquirer = order.payment_acquirer_id
-        return request.render('theme_inventive.inventive_congratulations_page', {
+        
+        # En Odoo 13, el payment_acquirer_id no existe directamente en sale.order
+        # Necesitamos obtenerlo a través de las transacciones de pago
+        if order:
+            # Buscar transacciones de pago relacionadas con esta orden
+            payment_transactions = request.env['payment.transaction'].sudo().search([
+                ('sale_order_ids', 'in', [order.id])
+            ], limit=1)
+            
+            if payment_transactions:
+                acquirer = payment_transactions.acquirer_id
+        
+        return request.render('certifica_theme.payment_confirmation_page', {
             'order': order,
             'acquirer': acquirer,
-        }) 
+        })
