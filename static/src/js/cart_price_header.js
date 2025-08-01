72bdd56a6eadfdb80071c86cefd7d6b3dfe6c9c6
Lorenzo <lorenrocu@gmail.com>
2025-08-01 10:43:39 -0500

    Update cart_price_header.js

--- static/src/js/cart_price_header.js
+++ static/src/js/cart_price_header.js
@@ -1,41 +1,41 @@
-odoo.define('certifica_theme.cart_price_header', function (require) {
-    'use strict';
-
-    var publicWidget = require('web.public.widget');
-
-    publicWidget.registry.CartPriceHeader = publicWidget.Widget.extend({
-        selector: '.oe_website_sale .oe_cart',
-        
-        start: function () {
-            var self = this;
-            return this._super.apply(this, arguments).then(function () {
-                // Verificar que estamos en la página del carrito
-                if (window.location.pathname === '/shop/cart') {
-                    self._updatePriceHeader();
-                }
-            });
-        },
-
-        _updatePriceHeader: function () {
-            // Buscar el encabezado de precio en la tabla del carrito
-            var $priceHeader = this.$('th.text-center.td-price');
+// JavaScript simple para cambiar el texto del encabezado de precio en el carrito
+$(document).ready(function() {
+    // Verificar que estamos en la página del carrito
+    if (window.location.pathname === '/shop/cart') {
+        // Función para actualizar el encabezado de precio
+        function updatePriceHeader() {
+            // Buscar el encabezado de precio con diferentes selectores
+            var $priceHeader = $('th.text-center.td-price');
             
             if ($priceHeader.length > 0) {
-                // Cambiar el texto de "Precio" a "Precio Unitario"
                 $priceHeader.text('Precio Unitario');
-                console.log('Texto del encabezado de precio actualizado a "Precio Unitario"');
-            } else {
-                // Intentar con otros selectores posibles
-                var $altPriceHeader = this.$('th:contains("Precio")');
-                if ($altPriceHeader.length > 0) {
-                    $altPriceHeader.text('Precio Unitario');
-                    console.log('Texto del encabezado de precio actualizado a "Precio Unitario" (selector alternativo)');
-                } else {
-                    console.log('No se encontró el encabezado de precio en la tabla del carrito');
-                }
+                console.log('Encabezado actualizado: Precio Unitario');
+                return;
             }
+            
+            // Selector alternativo
+            $priceHeader = $('th.td-price');
+            if ($priceHeader.length > 0) {
+                $priceHeader.text('Precio Unitario');
+                console.log('Encabezado actualizado: Precio Unitario (selector alternativo)');
+                return;
+            }
+            
+            // Buscar por contenido de texto
+            $('th').each(function() {
+                if ($(this).text().trim() === 'Precio') {
+                    $(this).text('Precio Unitario');
+                    console.log('Encabezado actualizado: Precio Unitario (por contenido)');
+                    return false; // Salir del each
+                }
+            });
         }
-    });
-
-    return publicWidget.registry.CartPriceHeader;
+        
+        // Ejecutar inmediatamente
+        updatePriceHeader();
+        
+        // También ejecutar después de un pequeño delay por si la página se carga dinámicamente
+        setTimeout(updatePriceHeader, 500);
+        setTimeout(updatePriceHeader, 1000);
+    }
 });
