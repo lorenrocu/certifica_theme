8e0c2bbd16e1587e40533ded0a791e1ed5093ee6
Lorenzo <lorenrocu@gmail.com>
2025-08-15 17:37:53 -0500

    correccion de modelos

--- __manifest__.py
+++ __manifest__.py
@@ -5,8 +5,9 @@
     'version': '13.0.1.0.0',
     'author': 'Lorenzo Romero',
     'website': 'https://helydev.com',
-    'depends': ['website', 'website_sale'],
+    'depends': ['website', 'website_sale', 'base'],
     'data': [
+        # Vistas
         'views/assets.xml',
         'views/layout.xml',
         'views/shop_layout.xml',
